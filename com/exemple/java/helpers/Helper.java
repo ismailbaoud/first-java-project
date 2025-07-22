00d3dcaf319f3954018c69414bed97875b15aeff
ismail baoud <ibaoud46@gmail.com>
2025-07-22 15:59:15 +0100

    ajouter le class dataType

---
+++ com/exemple/java/helpers/Helper.java
@@ -0,0 +1,23 @@
+public class Helper{
+    public static void main(String[] args) {
+        // short => Short
+        // byte => Byte
+        // char => Character
+        // int => Integer
+        // long => Long
+        // float => Float
+        // double => Double
+        System.out.println("min value of short is "+ Short.MIN_VALUE+"");
+        System.out.println("MAX value of short is "+ Short.MAX_VALUE+"\n");
+        System.out.println("min value of Byte is "+ Byte.MIN_VALUE+"");
+        System.out.println("MAX value of Byte is "+ Byte.MAX_VALUE+"\n");
+        System.out.println("min value of Integer is "+ Integer.MIN_VALUE+"");
+        System.out.println("MAX value of Integer is "+ Integer.MAX_VALUE+"\n");
+        System.out.println("min value of Long is "+ Long.MIN_VALUE+"");
+        System.out.println("MAX value of Long is "+ Long.MAX_VALUE+"\n");
+        System.out.println("min value of Float is "+ Float.MIN_VALUE+"");
+        System.out.println("MAX value of Float is "+ Float.MAX_VALUE+"\n");
+        System.out.println("min value of Double is "+ Double.MIN_VALUE+"");
+        System.out.println("MAX value of Double is "+ Double.MAX_VALUE+"\n");
+    }
+}
