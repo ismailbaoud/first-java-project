2fba52251c460d784a4c631c0f87b25f24bafadc
ismail baoud <ibaoud46@gmail.com>
2025-07-22 15:59:27 +0100

    ajouter le class Helpers

---
+++ com/exemple/java/data-types/dataTypes.java
@@ -0,0 +1,22 @@
+// byte 8byts  
+// short 16bits
+// int 32bits
+// long 64bits
+
+
+// float 
+// double 
+
+
+public class dataTypes{
+
+    public static void main(String args[]){
+        byte a = 1;  
+        short b = 2;
+        int c = 3;
+        long d = 4l;
+        float e = 5f;
+        double f = 6d;
+        System.out.println("n1 : "+ a+ "\nn2 : "+ b+ "\nn3 : "+ c+ "\nn4 : "+ d+ "\nn5 : "+ e +"\nn6 : "+ f  );
+    }
+}
