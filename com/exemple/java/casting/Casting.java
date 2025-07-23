5ab5fa17822bce50371cfd3657e9e5338a522539
ismail baoud <ibaoud46@gmail.com>
2025-07-23 21:16:36 +0100

    ajouter le class casting

---
+++ com/exemple/java/casting/Casting.java
@@ -0,0 +1,15 @@
+public class Casting {
+    public static void main(String args[]){
+        // up casting =>  widning 
+        // down casting => nearwing
+
+        short value = 128;
+
+        System.out.println("int value = " + value);
+
+        long longvalue = value;
+
+        System.out.println("long value = " + longvalue);
+        
+    }
+}
